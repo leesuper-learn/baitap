69ebfce8325a839470e17d368956d9a7ddad601e
Sillver Bullet <161559713+leesuper-learn@users.noreply.github.com>
2024-02-28 17:40:23 +0800

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap so tien USD: ");
+            int USD = int.Parse(Console.ReadLine());
+            int rate = USD * 23000;
+            Console.WriteLine("So tien VND la: " + rate);
+            Console.ReadKey();
+        }
+    }
+}
